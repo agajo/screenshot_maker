296b294e1438c47be76722ec974d587b9bbb6d7f
agajo <madaopc@gmail.com>
2020-10-28 14:46:13 +0900

    made 2 main widgets.

--- lib/screenshot_maker.dart
+++ lib/screenshot_maker.dart
@@ -1,7 +1,101 @@
 library screenshot_maker;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:async';
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+
+class ScreenshotMaker extends StatelessWidget {
+  const ScreenshotMaker(
+      {@required this.size,
+      @required this.outputFile,
+      @required this.child,
+      this.completer});
+  final Size size;
+  final Widget child;
+  final File outputFile;
+  final Completer<void> completer;
+  static GlobalKey renderRepaintBoundaryKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+      // Because it takes some time to rasterize, here is an artificial delay of 2 seconds.
+      // See the following link.
+      // https://api.flutter.dev/flutter/flutter_driver/FlutterDriver/screenshot.html
+      await Future.delayed(Duration(seconds: 2));
+      final rrb = ScreenshotMaker.renderRepaintBoundaryKey.currentContext
+          .findRenderObject() as RenderRepaintBoundary;
+      final imgData = await rrb.toImage();
+      final byteData = await imgData.toByteData(format: ImageByteFormat.png);
+
+      final pngBytes = byteData.buffer.asUint8List();
+      outputFile.writeAsBytesSync(pngBytes);
+      completer.complete();
+    });
+
+    // Thanks to this FittedBox, you can use Debug Paint on a simulator.
+    return FittedBox(
+      child: ConstrainedBox(
+        constraints: BoxConstraints(
+          maxWidth: size.width,
+          minWidth: size.width,
+          maxHeight: size.height,
+          minHeight: size.height,
+        ),
+        child: MediaQuery(
+          data: MediaQueryData(size: size),
+          child: RepaintBoundary(
+            key: ScreenshotMaker.renderRepaintBoundaryKey,
+            child: child,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Simulated extends StatelessWidget {
+  const Simulated(
+      {@required this.deviceFrameImage,
+      @required this.innerScreenSize,
+      @required this.innerScreenOffset,
+      @required this.child,
+      Size originalScreenSize})
+      : originalScreenSize = originalScreenSize ?? innerScreenSize;
+  final File deviceFrameImage;
+  final Size innerScreenSize;
+  final Size innerScreenOffset;
+  final Size originalScreenSize;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [
+      Padding(
+        padding: EdgeInsets.only(
+          left: innerScreenOffset.width,
+          top: innerScreenOffset.height,
+        ),
+        child: SizedBox(
+          width: innerScreenSize.width,
+          height: innerScreenSize.height,
+          child: FittedBox(
+            child: ConstrainedBox(
+              child: child,
+              constraints: BoxConstraints(
+                minWidth: originalScreenSize.width,
+                maxWidth: originalScreenSize.width,
+                minHeight: originalScreenSize.height,
+                maxHeight: originalScreenSize.height,
+              ),
+            ),
+          ),
+        ),
+      ),
+      Image.file(deviceFrameImage),
+    ]);
+  }
 }
