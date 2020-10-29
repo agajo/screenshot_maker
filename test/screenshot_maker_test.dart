66d0d6cb83688e49e17e28be8f352905e1c56e5c
agajo <madaopc@gmail.com>
2020-10-29 19:20:41 +0900

    wrote a test case, but it doesn't work.

--- test/screenshot_maker_test.dart
+++ test/screenshot_maker_test.dart
@@ -1,13 +1,36 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 
 import 'package:screenshot_maker/screenshot_maker.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
-  });
+  // This test doesn't work!! Please give me some hints to test this package.
+  testWidgets('check red 1 dot output', (WidgetTester tester) async {
+    final completer = Completer<void>();
+    final outputFile = MockFile();
+    await tester.pumpWidget(
+        ScreenshotMaker(
+          completer: completer,
+          outputFile: outputFile,
+          size: Size(1, 1),
+          child: MaterialApp(
+            home: Container(
+              width: 1,
+              height: 1,
+              color: Color.fromARGB(255, 255, 0, 0),
+            ),
+          ),
+        ),
+        Duration(seconds: 3));
+    await tester.pump(Duration(seconds: 3));
+    await expect(completer.future, completes);
+    await verify(outputFile.writeAsBytesSync(any));
+    // Skips this test because it doesn't work.
+  }, skip: true);
 }
+
+class MockFile extends Mock implements File {}
